4f45732c5f446d65da65dac798bcf7682c323823
Adam Pacholski <apachols@gmail.com>
2020-04-10 18:27:09 -0700

    Add unique ID to turn

--- src/webapp/features/rabble/TurnList.tsx
+++ src/webapp/features/rabble/TurnList.tsx
@@ -9,15 +9,11 @@ type TurnListProps = {
 const TurnList = (props: TurnListProps) => {
   const { turns } = props;
 
-  // Turn needs a unique ID...
-  const key = (turn: Turn) =>
-    `${turn.playerID}-${turn.tiles.map(t => t.letter)}`;
-
   return (
     <div>
       <ul className={styles.turnList}>
         {turns.map(t => {
-          return <Turn key={key(t)} turn={t} />;
+          return <Turn key={t.turnID} turn={t} />;
         })}
       </ul>
     </div>
