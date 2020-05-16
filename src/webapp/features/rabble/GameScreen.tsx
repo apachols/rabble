8ff29ffae6b2e6995f24660b74ea279c13303f52
Adam Pacholski <apachols@gmail.com>
2020-05-16 14:04:57 -0700

    Copy share link button v1

--- src/webapp/features/rabble/GameScreen.tsx
+++ src/webapp/features/rabble/GameScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./GameScreen.module.css";
 import { useParams } from "react-router-dom";
 
@@ -15,16 +15,39 @@ const GameScreen = (props: GameBoardProps) => {
 
   const displayScores = gameover ? gameover.finalScores : scores;
 
+  const inputRef: React.RefObject<HTMLInputElement> = useRef(null);
+
   return (
     <div className={styles.board}>
-      <GameControls nowPlaying={currentPlayer} {...props} />
-
       <div>
-        <span>
-          <strong>Invite a friend: </strong>
-        </span>
-        <div>{`${window.location.origin}/join/${gameID}`}</div>
+        <input
+          readOnly
+          ref={inputRef}
+          className={styles.clipboardInput}
+          value={`${window.location.origin}/join/${gameID}`}
+        />
+        <button
+          onClick={(event) => {
+            const theInput = inputRef.current as HTMLInputElement;
+            if (navigator.userAgent.match(/ipad|iphone/i)) {
+              const range = document.createRange();
+              range.selectNodeContents(theInput);
+              const selection = window.getSelection();
+              selection?.removeAllRanges();
+              selection?.addRange(range);
+              theInput.setSelectionRange(0, 999999);
+            } else {
+              theInput.select();
+            }
+            document.execCommand("copy");
+            theInput.blur();
+          }}
+        >
+          <strong>Invite a friend! (Click to copy) </strong>
+        </button>
       </div>
+
+      <GameControls nowPlaying={currentPlayer} {...props} />
 
       <h5 className={styles.subheading}>Scores</h5>
       <ul className={styles.scoreList}>
