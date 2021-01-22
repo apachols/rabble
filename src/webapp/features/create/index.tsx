833ba4cc3f2eca89afc99fe0f5af00cbd404b875
Evan Pacholski <epacholski86@gmail.com>
2021-01-22 09:52:04 -0800

    switched to Redirect component conditional for create game

--- src/webapp/features/create/index.tsx
+++ src/webapp/features/create/index.tsx
@@ -4,52 +4,49 @@ import axios from "axios";
 import { createUserGame } from "../../app/localStorage";
 import Loader from "react-loader-spinner";
 
-import { withRouter } from "react-router-dom";
-import { History } from "history";
+import { withRouter, Redirect } from "react-router-dom";
 
 const API_ROOT = `${process.env?.REACT_APP_API_ROOT || ""}`;
 
-interface CreateGameProps {
-  history: History;
-}
-
-const postToCreateGame = async () => {
-  const res = await axios({
-    method: "post",
-    headers: {
-      "content-type": "application/json",
-    },
-    url: `${API_ROOT}/games/rabble/create`,
-    data: {
-      setupData: {},
-      numPlayers: 2,
-    },
-  });
-
-  const { matchID } = res.data;
-
-  if (matchID) {
-    createUserGame(matchID);
-  } else {
-    throw new Error("Game ID not returned from server");
-  }
-
-  return matchID;
-};
-
-const CreateGame = ({ history }: CreateGameProps) => {
+const CreateGame = () => {
   const [loading, setLoading] = useState(false);
+
+  const [gameID, setGameID] = useState("");
+
+  const postToCreateGame = async () => {
+    const res = await axios({
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      url: `${API_ROOT}/games/rabble/create`,
+      data: {
+        setupData: {},
+        numPlayers: 2,
+      },
+    });
+
+    const { matchID } = res.data;
+
+    if (matchID) {
+      createUserGame(matchID);
+    } else {
+      throw new Error("Game ID not returned from server");
+    }
+    setGameID(matchID);
+  };
+
   return (
     <div className={styles.form}>
       <h3>Create a new game</h3>
+      {!!gameID && <Redirect to={`/join/${gameID}`} />}
       {loading ? (
         <Loader type="Grid" color="#00BFFF" height={100} width={100} />
       ) : (
         <button
           onClick={() => {
             setLoading(true);
-            const idtoPush = postToCreateGame();
-            history.push(`/join/${idtoPush}`);
+            postToCreateGame();
           }}
         >
           create
--- src/webapp/features/join/index.tsx
+++ src/webapp/features/join/index.tsx
@@ -14,100 +14,15 @@ import Loader from "react-loader-spinner";
 const API_ROOT = `${process.env?.REACT_APP_API_ROOT || ""}`;
 
 // TO TRY:
-// 1. pass the history object through the useEffect and posttojoin/redirectToGameView
+// 1. pass the history object through the useEffect and posttojoin/redirectToGameView -> something in the chain of passing skipped a beat, seems like it bailed on the post.
 // 2. bring redirecttogameview into the component for access to history
 //
 
-interface JoinGameProps {
-  history: History;
-}
+// interface JoinGameProps {
+//   history: History;
+// }
 
-const redirectToGameView = (gameID: string | undefined, history: History) => {
-  // TODO, react router recommends doing this with <Redirect> instead
-  // window.location.href = `/game/${gameID}`;
-  history.push(`/game/${gameID}`);
-};
-
-const getGameInfo = async (gameID: string) => {
-  const getResult = await axios({
-    method: "get",
-    headers: {
-      "content-type": "application/json",
-    },
-    url: `${API_ROOT}/games/rabble/${gameID}`,
-  });
-
-  return getResult.data;
-};
-
-const getOpenSeat = (gameInfo: any): string | null => {
-  const players: ServerPlayerMetadata[] = gameInfo.players;
-  const openSeatsOrderedByPlayerID = players
-    .filter((p) => !p.name)
-    .sort((p) => p.id);
-  const nextOpenSeat = openSeatsOrderedByPlayerID[0];
-  if (nextOpenSeat) {
-    return String(nextOpenSeat.id);
-  }
-  return null;
-};
-
-const getSeatedPlayerIDForNickname = (
-  gameInfo: any,
-  nickname: string
-): string | null => {
-  const players: ServerPlayerMetadata[] = gameInfo.players;
-  const existingSeatForNickname = players.find(({ name }) => name === nickname);
-  if (existingSeatForNickname) {
-    return String(existingSeatForNickname.id);
-  }
-  return null;
-};
-
-const getExistingOrNewPlayerID = (gameInfo: any, nickname: string): string => {
-  const playerID =
-    getSeatedPlayerIDForNickname(gameInfo, nickname) || getOpenSeat(gameInfo);
-  if (!playerID) {
-    throw new Error("Game is already full");
-  }
-  return playerID;
-};
-
-const postToJoinGame = async (
-  gameID: string | undefined,
-  nickname: string,
-  history: History
-) => {
-  if (!gameID) {
-    throw new Error("Missing gameID in JoinGame");
-  }
-
-  const gameInfo = await getGameInfo(gameID);
-
-  const playerID = getExistingOrNewPlayerID(gameInfo, nickname);
-
-  const postResult = await axios({
-    method: "post",
-    headers: {
-      "content-type": "application/json",
-    },
-    url: `${API_ROOT}/games/rabble/${gameID}/join`,
-    data: {
-      playerID,
-      playerName: nickname,
-    },
-  });
-
-  const { playerCredentials } = postResult.data;
-
-  updateUserNickName(nickname);
-
-  joinUserGame(gameID, playerID, playerCredentials);
-
-  redirectToGameView(gameID, history);
-};
-
-const JoinGame = ({ history }: JoinGameProps) => {
+const JoinGame = () => {
   const { gameID } = useParams();
   const [nickname, setNickname] = useState(getUserInfo().nickname);
   const [joinError, setJoinError] = useState("");
@@ -115,24 +30,111 @@ const JoinGame = ({ history }: JoinGameProps) => {
 
   const gameInfo = getPlayerGame(gameID);
 
+  const redirectToGameView = (gameID: string | undefined) => {
+    window.location.href = `/game/${gameID}`;
+  };
+
+  const getGameInfo = async (gameID: string) => {
+    const getResult = await axios({
+      method: "get",
+      headers: {
+        "content-type": "application/json",
+      },
+      url: `${API_ROOT}/games/rabble/${gameID}`,
+    });
+
+    return getResult.data;
+  };
+
+  const getOpenSeat = (gameInfo: any): string | null => {
+    const players: ServerPlayerMetadata[] = gameInfo.players;
+    const openSeatsOrderedByPlayerID = players
+      .filter((p) => !p.name)
+      .sort((p) => p.id);
+    const nextOpenSeat = openSeatsOrderedByPlayerID[0];
+    if (nextOpenSeat) {
+      return String(nextOpenSeat.id);
+    }
+    return null;
+  };
+
+  const getSeatedPlayerIDForNickname = (
+    gameInfo: any,
+    nickname: string
+  ): string | null => {
+    const players: ServerPlayerMetadata[] = gameInfo.players;
+    const existingSeatForNickname = players.find(
+      ({ name }) => name === nickname
+    );
+    if (existingSeatForNickname) {
+      return String(existingSeatForNickname.id);
+    }
+    return null;
+  };
+
+  const getExistingOrNewPlayerID = (
+    gameInfo: any,
+    nickname: string
+  ): string => {
+    const playerID =
+      getSeatedPlayerIDForNickname(gameInfo, nickname) || getOpenSeat(gameInfo);
+    if (!playerID) {
+      throw new Error("Game is already full");
+    }
+    return playerID;
+  };
+
+  const postToJoinGame = async (
+    gameID: string | undefined,
+    nickname: string
+  ) => {
+    if (!gameID) {
+      throw new Error("Missing gameID in JoinGame");
+    }
+
+    const gameInfo = await getGameInfo(gameID);
+
+    const playerID = getExistingOrNewPlayerID(gameInfo, nickname);
+
+    const postResult = await axios({
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      url: `${API_ROOT}/games/rabble/${gameID}/join`,
+      data: {
+        playerID,
+        playerName: nickname,
+      },
+    });
+
+    const { playerCredentials } = postResult.data;
+
+    updateUserNickName(nickname);
+
+    joinUserGame(gameID, playerID, playerCredentials);
+
+    redirectToGameView(gameID);
+  };
+
   useEffect(() => {
     if (gameInfo?.playerID && gameInfo?.playerCredentials) {
-      redirectToGameView(gameID, history);
+      redirectToGameView(gameID);
     }
-  }, [gameInfo, gameID, history]);
+  }, [gameInfo, gameID]);
 
   useEffect(() => {
     if (loading) {
       (async function asyncWrapper() {
         try {
-          await postToJoinGame(gameID, nickname, history);
+          await postToJoinGame(gameID, nickname);
         } catch (err) {
           setJoinError(`Failed to join ${gameID}`);
           setLoading(false);
         }
       })();
     }
-  }, [loading, gameID, nickname, history]);
+  }, [loading, gameID, nickname]);
 
   return (
     <div className={styles.form}>
