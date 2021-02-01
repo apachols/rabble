d687da302acf5b8dc903634c87fb97b7c9ffd9b8
Evan Pacholski <epacholski86@gmail.com>
2021-02-01 12:57:19 -0800

    introductory login screen added to pre.tsx in auth

--- src/webapp/features/auth/pre.tsx
+++ src/webapp/features/auth/pre.tsx
@@ -2,24 +2,33 @@ import React from "react";
 import styles from "./auth.module.css";
 import { useAuth } from "react-use-auth";
 
-const Login = () => {
-
-  const { isAuthenticated, login, logout } = useAuth();
-
-  console.log(isAuthenticated);
-
-  if (isAuthenticated()) {
-    return <button onClick={logout}>Logout</button>;
-  } else {
-    return <button onClick={login}>Login</button>;
-  }
-};
-
 const PreAuthentication = () => {
+  const { login } = useAuth();
+  // const { isAuthenticated, login } = useAuth();
 
   return (
     <div className={styles.container}>
-      <Login />
+      <h1>Sign In to Rabble</h1>
+      {/* TODO: this returns true even when not authenticated yet{isAuthenticated ? "true" : "false"} */}
+      <button
+        onClick={() => {
+          login();
+        }}
+      >
+        <span>Sign In</span>
+      </button>
+      <br></br>
+
+      <div className={styles.loginTilesContainer}>
+        <div className={styles.loginTile}>O</div>
+        <div className={styles.loginTile}>R</div>
+      </div>
+      <hr></hr>
+
+      <button>
+        <span>Sign Up</span>
+      </button>
+      <p>Rabble utilizes Auth0</p>
     </div>
   );
 };
